b8199621b47317ad44c4508d43ade54db765c60d
Alone Stand Larka <165143338+AloneStandLarkaAnas@users.noreply.github.com>
2024-08-27 10:08:00 -0400

    LiteLogo.sh

--- src/LiteLogo.sh
+++ src/LiteLogo.sh
@@ -6,8 +6,7 @@ _________            ___.                        ___________      .__        __
 \     \____  \___  |  | \_\ \ \  ___/   |  | \/    |    |   |  | \/  \  \___|    <  \___ \ 
  \______  /  / ____|  |___  /  \___  >  |__|       |____|   |__|  |__|\___  >__|_ \/____  >
         \/   \/           \/       \/                                     \/     \/     \/ 
-[+] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
-[+] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-[+] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+[+] Title    : AllHackingTools - Tool for hacking  - 
+[+] Coded By : Alone Stand Larka(Anas Abdullah)  -  All materials are provided for educational purposes only! 
 [+]———————-——-–————————-—-–———-——————-—–————-————-—–———————-——–—————–-——[+]'
 
